6e149d120b194a8db12d64a4f41359af5e4b9f53
Leonardo Eifert Catanante <leonardo.catanante@anvisa.gov.br>
2020-04-05 14:34:28 -0300

    Initial commit

---
+++ src/test/java/br/com/tcc/TCC/TccApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcc.TCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
